13158cc0df1070ef5f97a37756b3ed766fa42814
William <willyorrala8@gmail.com>
2024-01-06 16:04:26 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ImagePro"
+include(":app")
+include(":app:opencvlibrary3413")
